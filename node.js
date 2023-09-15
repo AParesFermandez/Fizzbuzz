7e80543e62b393647805cb60111946d73377adf7
AParAPares <amandapares87@gmail.com>
2023-09-15 10:54:12 -0300

    fizzbuzz

---
+++ node.js
@@ -0,0 +1,25 @@
+
+function fizzBuzz() {
+    var output = document.getElementById('output');
+
+    for (var i = 1; i <= 100; i++) {
+        var result = '';
+
+        if (i % 3 === 0) {
+            result += 'Fizz';
+        }
+
+        if (i % 5 === 0) {
+            result += 'Buzz';
+        }
+
+        if (result === '') {
+            result = i;
+        }
+
+        var div = document.createElement('div');
+        div.textContent = result;
+        output.appendChild(div);
+    }
+}
+fizzBuzz();
